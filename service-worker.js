0dd59d0705c2c1fcb4e5db92f9f7978029971613
Soare Robert-Daniel <robert.gamedev@pm.me>
2025-07-17 21:34:11 +0300

    cache management
    Summary:
    
    Test Plan:

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,53 @@
-// const CACHE_NAME = "v2";
-// const ASSETS_TO_CACHE = ["/assets/favicon.ico", "/van.min.js", "/main.mjs"];
+const CACHE_NAME = "v2";
+const ASSETS_TO_CACHE = ["/assets/favicon.ico", "/van.min.js", "/main.mjs"];
 
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       console.log("Cache non-critical assets");
-//       return cache.addAll(ASSETS_TO_CACHE);
-//     })
-//   );
-// });
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Cache non-critical assets");
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
 
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((cachedResponse) => {
-//       if (!cachedResponse) {
-//         return fetch(event.request).then((response) => {
-//           const responseToCache = response.clone();
-//           caches
-//             .open(CACHE_NAME)
-//             .then((cache) => cache.put(event.request, responseToCache));
-//           return response;
-//         });
-//       }
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      const url = new URL(event.request.url);
+      const isAssetToCache = ASSETS_TO_CACHE.includes(url.pathname);
 
-//       const responseDate = new Date(cachedResponse.headers.get("date"));
-//       const ageInMilliseconds = Date.now() - responseDate.getTime();
+      if (!isAssetToCache) {
+        // Not an asset to cache, just fetch from network
+        return fetch(event.request);
+      }
 
-//       if (ageInMilliseconds > 12 * 60 * 60 * 1000) {
-//         console.log(
-//           "Cache expired, fetching new data for " + event.request.url
-//         );
-//         return fetch(event.request).then((response) => {
-//           const responseToCache = response.clone();
-//           caches
-//             .open(CACHE_NAME)
-//             .then((cache) => cache.put(event.request, responseToCache));
-//           return response;
-//         });
-//       }
+      if (!cachedResponse) {
+        return fetch(event.request).then((response) => {
+          const responseToCache = response.clone();
+          caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.put(event.request, responseToCache));
+          return response;
+        });
+      }
 
-//       return cachedResponse;
-//     })
-//   );
-// });
+      const responseDate = new Date(cachedResponse.headers.get("date"));
+      const ageInMilliseconds = Date.now() - responseDate.getTime();
+
+      if (ageInMilliseconds > 12 * 60 * 60 * 1000) {
+        console.log(
+          "Cache expired, fetching new data for " + event.request.url
+        );
+        return fetch(event.request).then((response) => {
+          const responseToCache = response.clone();
+          caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.put(event.request, responseToCache));
+          return response;
+        });
+      }
+
+      return cachedResponse;
+    })
+  );
+});
